3ddafebcb91f1b4e9d2dc320c982a8b0d169a369
pentiumKing <ramahawai87@gmail.com>
2023-08-05 21:45:14 -0400

    added a controller folder and controllers files

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,105 +1,19 @@
-const fs = require('fs');
 const express = require('express')
-
-//routes
-const tours = JSON.parse(
-    fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-  );
-  
-const getAllTours =(req, res) => {
-    console.log(req.requestTime);
-    res.json({
-      status: 'success',
-      requestedAt: req.requestTime,
-      result: tours.length,
-      data: {
-        tours,
-      },
-    });
-  };
-
-const getTour =(req, res) => {
-    //JS trick way of converting a number string to a number
-    const id = req.params.id * 1;
-    if (id > tours.length) {
-      return res.status(404).json({
-        status: 'fail',
-        message: 'invalid ID',
-      });
-    }
-    //create an array with the specified id in the parameter
-    const tour = tours.find((el) => el.id === id);
-    res.json({
-      status: 'success',
-      data: {
-        tour,
-      },
-    });
-  };
+const tourController = require('./../controller/tourController')
 
 
-const createTour = (req, res) => {
-    // console.log(req.body);
-    //creating an id for the new data
-    const newId = tours[tours.length - 1].id + 1;
-    const newTour = Object.assign({ id: newId }, req.body);
-    tours.push(newTour);
-    fs.writeFile(
-      `${__dirname}/dev-data/data/tours-simple.json`,
-      JSON.stringify(tours),
-      (err) => {
-        res.json({
-          status: 'success',
-          data: {
-            tour: newTour,
-          },
-        });
-      }
-    );
-  };
-
-
-const updateTour =(req, res) => {
-    if (req.params.id * 1 > tours.length) {
-      return res.status(404).json({
-        status: 'fail',
-        message: 'invalid ID',
-      });
-    }
-
-    res.status(200).json({
-      status: 'success',
-      data: {
-        tour: '<Updated tour here....>',
-      },
-    });
-  };
-
-
-const deleteTour = (req, res) => {
-    if (req.params.id * 1 > tours.length) {
-      return res.status(404).json({
-        status: 'fail',
-        message: 'invalid ID',
-      });
-    }
-    res.status(204).json({
-      status: 'success',
-      data: null,
-    });
-  };
 
 //Mounting the tour routers
 const router = express.Router();
 router
 .route('/')
-.get(getAllTours)
-.post(createTour);
+.get(tourController.getAllTours)
+.post(tourController.createTour);
 
 router
   .route('/:id')
-  .get(getTour)
-  .patch(updateTour)
-  .delete(deleteTour);
+  .get(tourController.getTour)
+  .patch(tourController.updateTour)
+  .delete(tourController.deleteTour);
 
   module.exports = router
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,57 +1,19 @@
 const express = require('express')
-
-
-//Users Routers
-const getAllUsers=(req,res)=>{
-    res.status(500).json({
-      status: 'error',
-      message: 'this route is not yet defined'
-    })
-  }
-  
-  const getUser=(req,res)=>{
-    res.status(500).json({
-      status: 'error',
-      message: 'this route is not yet defined'
-    })
-  }
-
-  const createUser=(req,res)=>{
-    res.status(500).json({
-      status: 'error',
-      message: 'this route is not yet defined'
-    })
-  }
-
-  const updateUser=(req,res)=>{
-    res.status(500).json({
-      status: 'error',
-      message: 'this route is not yet defined'
-    })
-  }
-
-  const deleteUser=(req,res)=>{
-    res.status(500).json({
-      status: 'error',
-      message: 'this route is not yet defined'
-    })
-  }
-
-
-
+const userController = require("./../controller/userController")
+const router = express.Router()
 
 //Mounting user Routers
-const router = express.Router()
+
 router
 .route('/')
-.get(getAllUsers)
-.post(createUser)
+.get(userController.getAllUsers)
+.post(userController.createUser)
 
 router
 .route('/:id')
-.get(getUser)
-.patch(updateUser)
-.delete(deleteUser)
+.get(userController.getUser)
+.patch(userController.updateUser)
+.delete(userController.deleteUser)
 
 
 module.exports = router
